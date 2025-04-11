b364b4d5ff4f066c6f690c16264d4db77d79d720
Saloni <salonipatel501@gmail.com>
2025-04-11 02:23:10 -0400

    reset the from,created the post detail page

---
+++ src/posts/static/posts/detail.js
@@ -0,0 +1,19 @@
+const backBtn = document.getElementById('back-btn')
+const url = window.location.href + "data/"
+const spinnerBox = document.getElementById('spinner-box')
+
+backBtn.addEventListener('click', ()=>{
+    history.back()
+})
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log(response)
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,8 +3,10 @@ from .views import (
      post_list_and_create,
      load_post_data_view,
      like_unlike_post,
+     post_detail,
+     post_detail_data_view
 
-     hello_world_view
+    
 )
 
 app_name = 'posts'
@@ -12,7 +14,10 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/', post_detail, name='post-detail'),
 
-    path('hello-world/', hello_world_view, name='hello-world'),
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+
+    
 ]
