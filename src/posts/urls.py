032d4d7b3cade8ab0b34af9ab7a3e6fd182f4226
Saloni <salonipatel501@gmail.com>
2025-04-11 12:08:16 -0400

    update post with ajax

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,7 +4,7 @@ from .views import (
      load_post_data_view,
      like_unlike_post,
      post_detail,
-     post_detail_data_view
+     post_detail_data_view,
      delete_post,
      update_post,
 
@@ -17,8 +17,8 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
-    path('<pk>/update', update_post, name='post-update'),
-    path('<pk>/delete', delete_post, name='post-delete'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
 
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
