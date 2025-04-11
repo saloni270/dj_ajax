65f0ffe8abb9b40c31099df1627c0a7b5de375d1
Saloni <salonipatel501@gmail.com>
2025-04-11 11:46:40 -0400

    wrote some code for update and delete views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -5,6 +5,8 @@ from .views import (
      like_unlike_post,
      post_detail,
      post_detail_data_view
+     delete_post,
+     update_post,
 
     
 )
@@ -15,6 +17,9 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/update', update_post, name='post-update'),
+    path('<pk>/delete', delete_post, name='post-delete'),
+
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
